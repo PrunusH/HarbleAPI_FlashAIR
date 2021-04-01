8383233c2acbabf10b0a1035fc98eed0bca4491d
Lilith <lilithrainbows@outlook.com>
2021-04-01 09:31:40 -0300

    Release 1

---
+++ HarbleAPI for FlashAIR/HarbleMessage.vb
@@ -0,0 +1,16 @@
+﻿Public Class HarbleMessage
+    Public Name As String
+    Public ID As String
+    Public Hash As String
+    Public IsOutgoing As Boolean
+    Public ClassName As String
+    Public ClassNamespace As String
+    Public Sub New(Name As String, ID As String, Hash As String, IsOutgoing As Boolean, ClassName As String, ClassNamespace As String)
+        Me.Name = Name
+        Me.ID = ID
+        Me.Hash = Hash
+        Me.IsOutgoing = IsOutgoing
+        Me.ClassName = ClassName
+        Me.ClassNamespace = ClassNamespace
+    End Sub
+End Class
---
+++ HarbleAPI for FlashAIR/NamespaceCache.vb
@@ -0,0 +1,9 @@
+﻿Public Class NamespaceCache
+    Public OldNamespaceName As String
+    Public NewNamespaceName As String
+
+    Public Sub New(OldNamespaceName As String, NewNamespaceName As String)
+        Me.OldNamespaceName = OldNamespaceName
+        Me.NewNamespaceName = NewNamespaceName
+    End Sub
+End Class
